d2953ca70412a630216df8eddc0af7cd91ed5126
ikuma-hiroyuki <chemicalbondo@gmail.com>
2020-11-11 13:08:11 +0900

    スタイルの書き出し

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+
+const double KbottomContainerHeight = 80;
+const Color KactiveCardColor = Color(0xFF1D1E33);
+const Color KinActiveCardColor = Color(0xFF111328);
+const Color KbottomContainerColor = Color(0xFFEB1555);
