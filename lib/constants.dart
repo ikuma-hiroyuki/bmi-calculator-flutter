db8904ec291a4098ce5afb8b99614b3cd5f30fe9
ikuma-hiroyuki <chemicalbondo@gmail.com>
2020-11-12 08:41:46 +0900

    テキストスタイルの変数名の変更

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
-const labelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
-
-const double KbottomContainerHeight = 80;
-const Color KactiveCardColor = Color(0xFF1D1E33);
-const Color KinActiveCardColor = Color(0xFF111328);
-const Color KbottomContainerColor = Color(0xFFEB1555);
+const KLabelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+const KNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+const double KBottomContainerHeight = 80;
+const Color KActiveCardColor = Color(0xFF1D1E33);
+const Color KInActiveCardColor = Color(0xFF111328);
+const Color KBottomContainerColor = Color(0xFFEB1555);
